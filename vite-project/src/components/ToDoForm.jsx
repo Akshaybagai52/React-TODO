d0e52b42e0f5fb30c64b4351e3135eebc8639277
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-08 23:31:00 +0530

    gg

--- vite-project/src/components/ToDoForm.jsx
+++ vite-project/src/components/ToDoForm.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ToDoForm = () => {
   return (
-    <div>ToDoForm</div>
+    <div className="p-4 bg-gray-200 rounded">
+      <h2 className="text-lg font-bold mb-4">Add Todo</h2>
+      <form className="flex items-center gap-4">
+        <label htmlFor="todoInput" className="font-semibold">Task:</label>
+        <input type="text" id="todoInput" className="px-2 py-1 border border-gray-300 rounded" />
+        <button type="submit" className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Add</button>
+      </form>
+    </div>
   )
 }
 
